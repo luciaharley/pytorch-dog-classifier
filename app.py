7bf28c5c0868ab838239f5a36952b7750411b362
Lucia Page-Harley <luciaharley1@gmail.com>
2021-08-10 21:50:40 -0700

    add demo photos

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ from utils import *
 st.set_page_config(layout="wide")
 st.title("Mixed-Breed Dog Classifier")
 st.sidebar.header('Welcome!')
-st.sidebar.markdown("To begin, upload any photo. Square or close to square photos will display better!")
+st.sidebar.markdown("To begin, upload any photo that's in the same directory as the app. Square or close to square photos will display better!")
+
 st.sidebar.markdown("In this project, we attempted to recreate the model described in the methods section of this [paper](https://www.academia.edu/33721767/Mixed_Breed_Dogs_Classification). First, we trained a fine-tuned Inception model on the Stanford Dogs Dataset. Specifically, we added an extra fully-connected layer and corresponding dropout layer to the existing model to assign scores to the 120 dog breeds. We then tested the model on our mixed-breed dataset, taking the top 2 highest predicted breeds as the putative parent breeds for each image.")
 
 # load breed encodings
@@ -65,7 +66,6 @@ if uploaded_file is not None:
         st.write("")
     st.write("")
     
-
     # run image through model
     inputs, _ = next(iter(single_test_loader))
     outputs = inception(inputs)
@@ -80,17 +80,17 @@ if uploaded_file is not None:
     parent2_row = demo_images_df.loc[demo_images_df['Label'].str.lower() == parent_breeds[1]]
 
     # display results
-    stanford_image_path = 'stanford-dogs-dataset/images/Images'
+    stanford_image_path = 'demo_photos'
     
     plus = cv2.imread('plus.png')
     plus = cv2.cvtColor(plus, cv2.COLOR_BGR2RGB)
     plus = cv2.resize(plus, (150,300))
     
-    image2 = cv2.imread(stanford_image_path + parent1_row.Path.values[0])
+    image2 = cv2.imread(stanford_image_path + parent1_row.Filename.values[0])
     image2 = cv2.cvtColor(image2, cv2.COLOR_BGR2RGB)
     image2 = cv2.resize(image2, (299,299))
 
-    image3 = cv2.imread(stanford_image_path + parent2_row.Path.values[0])
+    image3 = cv2.imread(stanford_image_path + parent2_row.Filename.values[0])
     image3 = cv2.cvtColor(image3, cv2.COLOR_BGR2RGB)
     image3 = cv2.resize(image3, (299,299))
 
